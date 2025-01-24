cdf2885d3d0c776c5ac64418db8e4241b0f9b633
Nico <nazucca7@gmail.com>
2025-01-24 13:14:52 -0700

    remove firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
-import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
